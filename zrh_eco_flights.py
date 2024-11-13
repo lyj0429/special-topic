a026cd905c5cfc66bd763f70a0d5155891f8256b
李詠潔 <s411178040@gm.ntpu.edu.tw>
2024-11-13 22:36:24 +0800

    zrh_eco_flights.py

--- zrh_eco_flights.py
+++ zrh_eco_flights.py
@@ -10,11 +10,23 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from datetime import datetime, timedelta
+from datetime import timedelta, datetime
 import time
 import csv
 import re
 import os
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+# 設置 Selenium 驅動
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-software-rasterizer")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=service, options=options)
+
 
 def calculate_dates(today_date_str):
     today = datetime.strptime(today_date_str, "%Y-%m-%d")
@@ -30,15 +42,6 @@ def calculate_dates(today_date_str):
 
     return start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d")
 
-# 設置 Selenium 驅動
-options = Options()
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-options.add_argument("--disable-software-rasterizer")
-options.add_argument("--headless")
-service = Service("/Users/lbb/Desktop/chromedriver-mac-arm64/chromedriver")
-driver = webdriver.Chrome(service=service, options=options)
 
 def scrape_flights(start_date_str, end_date_str):
     start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
